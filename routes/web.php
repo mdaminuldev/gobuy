0c06a42b99bc6fd9263d46124e7511118700ebba
duplidev <social@duplidev.tech>
2023-03-27 21:57:47 +0600

    feat(user): add user interface frontend page and HomeController
    - Added a new user interface frontend page for viewing the website
    - Implemented a route for the new user interface page
    - Created a new template for the user interface frontend page
    - Added a new HomeController to handle the new user interface page
    
    This commit adds a new user interface frontend page to the user module, allowing users to view the website. A new route has been created to handle this page, and a new template has been added to the application to enable users to view the website's content. Additionally, a new HomeController has been added to the application to handle requests related to this new user interface page.
    
    Closed #8

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\OrderController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\SubCategoryController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,9 +20,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::controller(HomeController::class)->group(function(){
+    Route::get('/','index')->name('home');
 });
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
